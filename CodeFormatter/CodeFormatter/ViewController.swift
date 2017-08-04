ba2b85938436e2d1ec62e80901214d2c87e51795
Victor Chee <victor.qhj@gmail.com>
2017-08-04 14:46:29 +0800

    Add project.

---
+++ CodeFormatter/CodeFormatter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeFormatter
+//
+//  Created by Migu on 2017/8/4.
+//  Copyright © 2017年 VIctorChee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
