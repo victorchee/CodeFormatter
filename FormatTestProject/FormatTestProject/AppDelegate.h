179993e70375e1cc927ff1ee8e7300840d093706
Victor Chee <victor.qhj@gmail.com>
2017-08-04 16:09:01 +0800

    Add test project.

---
+++ FormatTestProject/FormatTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FormatTestProject
+//
+//  Created by Migu on 2017/8/4.
+//  Copyright © 2017年 VIctorChee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
